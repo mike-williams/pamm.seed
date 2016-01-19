53ea014899bd0c2668694148bd57756f0bd27e63
jelly0@langstone.org.uk <jelly0@langstone.org.uk>
2016-01-19 08:23:51 +0000

    Using play Json mapperfor register user

--- svc/app/net/atos/pamm/domain/user/service/RegisterUserOperation.java
+++ svc/app/net/atos/pamm/domain/user/service/RegisterUserOperation.java
@@ -1,14 +1,15 @@
 package net.atos.pamm.domain.user.service;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import net.atos.pamm.domain.ServiceResult;
+import net.atos.pamm.domain.user.UserRepository;
+import net.atos.pamm.domain.user.model.User;
+import net.atos.pamm.infrastructure.mail.EmailService;
+import net.atos.pamm.infrastructure.security.cipher.BCryptCipher;
 import net.atos.pamm.infrastructure.util.RandomKeyGenerator;
 import net.atos.pamm.infrastructure.util.RequestUtil;
 import play.Logger;
-import net.atos.pamm.domain.ServiceResult;
-import net.atos.pamm.domain.user.model.User;
-import net.atos.pamm.domain.user.UserRepository;
-import net.atos.pamm.infrastructure.mail.EmailService;
-import net.atos.pamm.infrastructure.security.cipher.BCryptCipher;
+import play.libs.Json;
 
 import javax.inject.Inject;
 
@@ -39,14 +40,7 @@ public class RegisterUserOperation {
         if (existingUser != null) {
             return new ServiceResult(ServiceResult.Status.OP_ERROR);
         } else {
-            final User user = new User();
-
-            user.setEmail(jsonRequest.findPath("email").textValue());
-            user.setForename(jsonRequest.findPath("forename").textValue());
-            user.setSurname(jsonRequest.findPath("surname").textValue());
-            user.setJobTitle(jsonRequest.findPath("job_title").textValue());
-            user.setBaseSite(jsonRequest.findPath("base_site").textValue());
-            user.setPhone(jsonRequest.findPath("phone").textValue());
+            final User user = Json.fromJson(jsonRequest, User.class);
 
             user.setPassword(cipher.hash(jsonRequest.findPath("password").textValue()));
             user.setActivated(false);
